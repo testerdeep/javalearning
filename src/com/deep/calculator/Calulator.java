578b297779f119fad63e1b545a0d3177b4847fe3
Deep Singh Verma <deepsv@citruspayho.com>
2016-08-11 10:00:02 +0530

    Learning Regex and writing basic calculator

---
+++ src/com/deep/calculator/Calulator.java
@@ -0,0 +1,16 @@
+package com.deep.calculator;
+
+import java.io.InputStreamReader;
+
+public class Calulator {
+	
+	public static void main(String args[]){
+		
+		System.out.println("Welcome to the Calculation world!!!!!");
+		System.out.println("Please select the operation you want to perform");
+		System.out.println("1.Add \n2.Subtract \n3.Multiply \n4.Divide \n5.Max \n6.Min \n7.Average");
+	    System.out.println("Please select your option:");
+	    InputStreamReader cin= new InputStreamReader(System.in);
+	}
+ 
+}
---
+++ src/com/deep/training/LearningRegex.java
@@ -0,0 +1,42 @@
+package com.deep.training;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class LearningRegex {
+	
+	public static void main(String args[]){
+	
+	String line ="This 123 123 order was 3456 placed This  1 2 3for QT2k23 123 OK? 3456 uiui";
+	
+	String pattern="(This)";
+	
+	Pattern r = Pattern.compile(pattern);
+	Matcher m = r.matcher(line);
+	int i=0;
+	System.out.println(m.lookingAt());
+	
+	while (m.find()){
+		
+		System.out.println("groups:"+ m.groupCount());
+	
+		System.out.println("goup():" + m.group());
+		System.out.println ("  starting at index " + m.start () +
+                " and ending at index " + m.end ());
+		System.out.println ();
+		i++;
+		
+		
+	
+		//System.out.println("goup(1):" + m.group(1));
+		//System.out.println("goup(2):" + m.group(2));
+		//System.out.println("goup(3):" + m.group(3));
+		//System.out.println("goup(4):" + m.group(4));
+		//System.out.println("goup(5):" + m.group(5));
+		
+		
+		
+	}
+	System.out.println("Number of time occurences:"+ i);
+	
+	}	
+}
