2d3720dfc98b1237dd94f314c7be5565c1928a1b
Deep Verma <Deep Verma>
2016-08-21 08:57:44 +0530

    Adding calculator methods

---
+++ src/com/deep/calculator/Maths.java
@@ -0,0 +1,37 @@
+package com.deep.calculator;
+
+public interface Maths {
+
+	String  maths="This is world of mathmatics";
+	double sum(int sum[]);
+	double substraction(double firstnum,double secondnum);
+	double multiplication(double firstnum,double secondnum);
+	double division(double firstnum,double secondnum);
+	
+	default int sum(int num1,int num2){
+		int sum;
+		sum=num1+num2;
+		return sum;
+	}
+	
+	default int substraction(int num1,int num2){
+		int substraction;
+		substraction=num1 - num2;
+		return substraction;
+	}
+	
+	default int multiplication(int num1,int num2){
+		int product;
+		product=num1*num2;
+		return product;
+		
+	}
+	
+	default int division(int num1,int num2){
+		
+		int result;
+		result=num1/num2;
+		return result;
+	}
+	
+}
--- src/com/deep/calculator/Mathsmethod.java
+++ src/com/deep/calculator/Mathsmethod.java
@@ -1,12 +1,29 @@
 package com.deep.calculator;
 
-public class Mathsmethod {
+public class Mathsmethod implements Maths{
 	
-	double sum(){
+public double sum(int num[]){
 		
 		int sum=0;
 		
 		return sum;
 	}
+
+public double substraction(double firstnum,double secondnum ){
+	double substraction= firstnum - secondnum;
+	return substraction;
+	}
+
+public double multiplication(double firstnum,double secondnum){
+	double product;
+	product=firstnum*secondnum;
+	return product;
+}
+
+public double division(double firstnum,double secondnum){
+	double result;
+	result=firstnum/secondnum;
+	return result;
+}
 
 }
